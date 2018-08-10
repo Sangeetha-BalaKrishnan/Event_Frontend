267d8b86d0ae5fc972c3dcf4f7ca5d15c6672f78
Saravanan <ls.saravanan96@gmail.com>
2018-08-10 08:22:16 +0530

    friday

--- src/Signup.js
+++ src/Signup.js
@@ -4,6 +4,26 @@ import ReactDOM from 'react-dom';
 import logo from './images/new.png';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const after={
+  borderColor:'red'
+};
+
+const before={
+  borderColor:'#cccccc'
+}
+const message=(
+  <span style={{fontFamily:'Roboto'}}>&nbsp;Enter this field</span>
+);
+
+const password_msg=(
+  <span style={{fontFamily:'Roboto'}}>&nbsp;Passwords should be same</span>
+);
+
+const password_msg1=(
+  <span style={{fontFamily:'Roboto'}}>&nbsp;Atleast 8 min char</span>
+);
+
+
 class Signin extends Component{
   constructor(props){
     super(props);
@@ -12,29 +32,91 @@ class Signin extends Component{
       email:'',
       password:'',
       cpassword:'',
-      phone:''
+      phone:'',
+      check_name_user:before,
+      check_email:before,
+      check_password:before,
+      check_password:before,
+      check_phone:before,
+      pass_length:false,
+      pass_crct:false
 
   };
   this.handleChange = this.handleChange.bind(this);
+  this.handleChange_toggle = this.handleChange_toggle.bind(this);
   this.handleClick = this.handleClick.bind(this);
+  this.length_check = this.length_check.bind(this);
+  this.check_all = this.check_all.bind(this);
 
 }
+handleChange_toggle(event){
+
+  this.setState({[event.target.name] : event.target.value});
+
+var temp="check_"+event.target.name;
+console.log(temp);
+  if((event.target.value).length>0)
+  {
+    this.setState({[temp]:before});
+  }
+}
 handleChange(event){
-  this.setState({[event.target.name]:event.target.value});
-  ReactDOM.findDOMNode(this.refs.input).borderColor='red';
+  this.setState({[event.target.name] : event.target.value});
+  var temp="check_"+event.target.name;
+  if(event.target.value=='')
+  {
+    this.setState({[temp]:after});
+  }
 }
 handleClick()
 {
-  if(this.state.user_name!=='' && this.state.email!=='' && this.state.password!=='' && this.state.cpassword!=='' && this.state.phone!='')
+  if(this.state.name_user=='')
   {
-    if(this.state.passord!==this.state.cpassword)
+    this.setState({check_name_user:after});
+  }
+  if(this.state.email=='')
+  {
+    this.setState({check_email:after});
+  }
+  if(this.state.password=='')
+  {
+    this.setState({check_password:after});
+  }
+  if(this.state.cpassword=='')
+  {
+    this.setState({check_cpassword:after});
+  }
+  if(this.state.phone=='')
+  {
+    this.setState({check_phone:after});
+  }
+}
+
+length_check()
+{
+  if(this.state.password.length<8 && this.state.password!=='')
+  {
+    this.setState({check_password:after,pass_length:true});
+  }
+  if(this.state.password.length>=8)
+  {
+    this.setState({check_password:before,pass_length:false});
+  }
+}
+check_all()
+{
+  console.log(this.state.password,this.state.cpassword);
+    if( this.state.passord!==this.state.cpassword && this.state.password!=='')
     {
-      alert("both PW should be same");
+        this.setState({check_cpassword:after,pass_crct:true});
     }
-  }
-  else {
-    alert("fill all the details");
-  }
+
+
+    if(this.state.password==this.state.cpassword)
+    {
+        this.setState({check_cpassword:before,pass_crct:false});
+    }
+
 }
   render(){
     const hello={
@@ -66,7 +148,7 @@ handleClick()
       </Link>
       </div>
       <div id="bkground" class="row">
-      <ul id="signup22"><li id="list1"> Already a Member?</li><Link to={`/User/signin`}><li id="list2"><button class="btn btn-default" id="btt">signin</button></li></Link></ul>
+      <ul id="signup22"><li id="list1"> Already a Member?</li><Link to={`/User/cust/signin`}><li id="list2"><button class="btn btn-default" id="btt">signin</button></li></Link></ul>
 
 
       <div id="logform2" class="col-sm-6">
@@ -86,28 +168,33 @@ handleClick()
 
           <div id="cat" class="row">
             <div  class="col-sm-6">
-                <input name="name_user" ref="input" value={this.state.name_user} onChange={this.handleChange} type="text" placeholder="Name" id="email1" class="form-control" required/>
+                <input name="name_user" style={this.state.check_name_user} ref="input" value={this.state.name_user} onChange={this.state.check_name_user==after?this.handleChange_toggle:this.handleChange} type="text" placeholder="Name" id="email1" class="form-control" required/>
                 <label  class="form-control-placeholder" for="name">Name</label>
+                {this.state.check_name_user==after?message:''}
             </div>
             <div class="col-sm-6">
-                  <input name="email" value={this.state.email} onChange={this.handleChange} type="text"  placeholder="Email Address" id="email1" class="form-control" required/>
+                  <input name="email" style={this.state.check_email} value={this.state.email} onChange={this.state.check_email==after?this.handleChange_toggle:this.handleChange} type="text"  placeholder="Email Address" id="email1" class="form-control" required/>
                   <label class="form-control-placeholder" for="password">Email Address</label>
+                  {this.state.check_email==after?message:''}
               </div>
           </div>
           <div id="cat1" class="row">
             <div  class="col-sm-6">
-                <input name="password" value={this.state.password} onChange={this.handleChange} type="text" placeholder="Password" id="email1" class="form-control" required/>
+                <input name="password" style={this.state.check_password} value={this.state.password} onMouseOut={this.length_check} onChange={this.state.check_password==after?this.handleChange_toggle:this.handleChange} type="text" placeholder="Password" id="email1" class="form-control" required/>
                 <label  class="form-control-placeholder" for="name">Password</label>
+                {this.state.check_password==after?this.state.pass_length==true?password_msg1:message:''}
             </div>
             <div class="col-sm-6">
-                  <input name="cpassword" value={this.state.cpassword} onChange={this.handleChange} type="text"  placeholder="Confirm Password" id="email1" class="form-control" required/>
+                  <input name="cpassword" style={this.state.check_cpassword} value={this.state.cpassword} onMouseOut={this.check_all} onChange={this.state.check_cpassword==after?this.handleChange_toggle:this.handleChange} type="text"  placeholder="Confirm Password" id="email1" class="form-control" required/>
                   <label class="form-control-placeholder" for="password">Confirm Password</label>
+                  {this.state.check_cpassword==after?this.state.pass_crct==true?password_msg:message:''}
               </div>
           </div>
           <div id="cat1" class="row">
             <div  class="col-sm-6">
-                <input name="phone" value={this.state.phone} onChange={this.handleChange} type="number" placeholder="Phone Number" id="email1" class="form-control" required/>
+                <input name="phone" style={this.state.check_phone} value={this.state.phone} onChange={this.state.check_phone==after?this.handleChange_toggle:this.handleChange} type="number" placeholder="Phone Number" id="email1" class="form-control" required/>
                 <label  class="form-control-placeholder" for="name">Phone Number</label>
+                {this.state.check_phone==after?message:''}
             </div>
 
           </div>
